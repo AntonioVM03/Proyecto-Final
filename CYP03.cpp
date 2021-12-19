8728c0e33a647aeaa0be9b2fc66a5829d4a28e18
AntonioVM03 <antoniovillegas07@aragon.unam.mx>
2021-12-19 16:41:49 -0600

    Primer programa

---
+++ CYP03.cpp
@@ -0,0 +1,31 @@
+// CYP03.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main ()
+{
+		int dia = 0, mes = 0, ano = 0;
+
+		int meses[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+		scanf_s("%i %i %i", &dia, &mes, &ano);
+		if (dia == 4 && mes == 10 && ano == 1582) {
+		dia = 14;
+	}
+		if (((ano % 4) == 0) && (ano != 100)) {
+		meses[1] = 29;
+	} //31 11 2000 ----> 1 12 2000
+		if (dia == meses[mes - 1]) {
+		dia = 1;
+		if (mes == 12) {
+			mes = 1;
+			ano++;
+		}
+		else { mes++; }
+	}
+		else { dia++; }
+		printf("%2i %2i %4i", dia, mes, ano);
+		return 0;
+}
+
+
